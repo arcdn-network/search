33fbc289d87f1f390f232afe6148d02c7838c508
arcdn-network <pycharm113@gmail.com>
2026-04-13 14:03:06 -0500

    save changes

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const GRUPO_DEV = '-1003375877299';
-const GRUPO_PROD = '-1003422780175';
+const GRUPO_PROD = '-1003970423599';
 
 const IS_DEV = process.env.IS_DEV === 'TRUE';
 const GRUPO_ID = IS_DEV ? GRUPO_DEV : GRUPO_PROD;
